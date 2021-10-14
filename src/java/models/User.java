e85cab327edd0cd0b423cbb009fb102e97883cce
hazco <hazco@192.168.1.135>
2021-10-13 18:06:24 -0600

    Review User.java

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -9,9 +9,6 @@ public class User {
     private String username;
     private String password;
 
-    public User() {
-    }
-
     public User(String username, String password) {
         this.username = username;
         this.password = password;
